ea43e5376a276ca864af15625bfd6685d9191cdd
Ieva <ieva.mazet@gmail.com>
2026-04-23 21:01:26 +0100

    minor amendment

--- api/models/song.js
+++ api/models/song.js
@@ -4,19 +4,7 @@ const SongSchema = new mongoose.Schema({
   artist: { type: String, required: true },
   title: { type: String, required: true },
   year: { type: Number, required: true },
-  url: { 
-    type: String, 
-    required: true, 
-    validate: function(url) {
-      try {
-        new URL(url);
-        return true;
-      } catch (err) {
-        return false;
-      }
-    },
-    message: props => `${props.value} is not a valid URL`
-  }
+  url: { type: String, required: true }
 });
 
 const Song = mongoose.model("Song", SongSchema);
