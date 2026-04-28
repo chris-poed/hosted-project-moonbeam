a09d16da42f26e0e5f7f9b473698fef1347150ab
Alex Tanner <alextanner@fastmail.com>
2026-04-28 11:14:46 +0100

    adds more song ids to Songs.js for testing of timeline.

--- frontend/src/data/Songs.js
+++ frontend/src/data/Songs.js
@@ -2,4 +2,7 @@ export const songs = [
   { id: "1", title: "Bohemian Rhapsody", artist: "Queen", year: 1975 },
   { id: "2", title: "Smells Like Teen Spirit", artist: "Nirvana", year: 1991 },
   { id: "3", title: "Johnny B. Goode", artist: "Buddy Holly", year: 1958 },
+  { id: "4", title: "Bohemian Rhapsody", artist: "Queen", year: 1976 },
+  { id: "5", title: "Smells Like Teen Spirit", artist: "Nirvana", year: 1992 },
+  { id: "6", title: "Johnny B. Goode", artist: "Buddy Holly", year: 1959 },
 ]
