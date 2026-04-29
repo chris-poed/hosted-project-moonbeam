a8158849a88e2ba36ce0942735276c0c2aa1627f
chris-poed <christianpoed@gmail.com>
2026-04-29 22:23:24 +0100

    update GameScreen import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/Home/HomePage";
 import { Lobby } from "./pages/Lobby/LobbyPage";
-import { GameScreen } from "./pages/GameScreen";
+import { GameScreen } from "./pages/GameScreen/GameScreen";
 import { RevealPage } from "./pages/Reveal/RevealPage";
 import { GameOverPage } from "./pages/GameOver/GameOverPage"
 
