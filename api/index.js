79777881d230f699cf2259789666d818bdad5772
chris-poed <christianpoed@gmail.com>
2026-04-30 17:18:10 +0100

    update config for hosting

--- api/index.js
+++ api/index.js
@@ -6,11 +6,25 @@ const { Server } = require("socket.io");
 const { connectToDatabase } = require("./db/db.js");
 const app = express();
 const httpServer = createServer(app);
+
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://localhost:5174",
+].filter(Boolean);
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
+app.use(express.json());
+
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-  }
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+  },
 });
 
 const registerCreateGameHandlers = require("./sockets/createGameHandlers.js")
@@ -24,7 +38,8 @@ io.on("connection", onConnection);
 
 function listenForRequests() {
   const port = process.env.PORT || 3000;
-  httpServer.listen(port, () => {
+
+  httpServer.listen(port, "0.0.0.0", () => {
     console.log("Now listening on port", port);
   });
 }
