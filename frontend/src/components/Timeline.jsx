947639548d5245f3c2516823409fd1b9034a888b
chris-poed <christianpoed@gmail.com>
2026-05-01 13:33:41 +0100

    wip fix for timeline spacing

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,38 +1,31 @@
 import { useDroppable } from "@dnd-kit/core";
-import { SortableContext, horizontalListSortingStrategy, } from "@dnd-kit/sortable";
+import { SortableContext, rectSortingStrategy } from "@dnd-kit/sortable";
 import TimelineItem from "./TimelineItem";
+import "./Timeline.css";
 
 // timeline, activeTimelineCard and disabled are given default values now to prevent .map errors
 function Timeline({
-        timeline = [],
-        activeTimelineCard = null,
-        disabled = false,
+    timeline = [],
+    activeTimelineCard = null,
+    disabled = false,
     }) {
     const droppable = useDroppable({
-    id: "timeline",
-    disabled,
-    });  // useDroppable is making clear to dnd-kit that this box (timeline) accepts dropped cards and is called timeline
-                                                        // id: timeline later becomes over.id when a dragged card is hovering over it
+        id: "timeline",
+        disabled,
+    });
+
     return (
-        <div>
-        {/* <h2>Timeline</h2> */}
-        <div // because of ref={droppable.setNodeRef}, this div is where dropping is allowed
-            ref={droppable.setNodeRef} // connects the actual HTML element to dnd-kit.  Without this, useDroppable() would exist but would not be attached to anything
-            style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "10px",
-            minHeight: "80px",
-            border: "1px solid black",
-            padding: "10px",
-            }}
+        <div className="timeline-wrapper">
+        <div
+            ref={droppable.setNodeRef}
+            className="timeline-dropzone"
         >
-            <SortableContext // tells dnd-kit that these items or cards belong to sortable group. The timeline cards need SortableContext to move from side to side
-            items={timeline.map(song => song.id)} // uses .map here to take timeline array that is passed in from dndContext and transform data into just 
-            strategy={horizontalListSortingStrategy} // the timeline card ids.  dnd-kit needs to know that the cards are sortable and what their order is.
-            >                                           
-            {timeline.map(song => (             // strategy={horizontalListSortingStrategy} - shifts the cards from left to right
-                <TimelineItem                      // {props.timeline.map(song => ( This creates one TimelineItem for every song.
+            <SortableContext
+            items={timeline.map((song) => song.id)}
+            strategy={rectSortingStrategy}
+            >
+            {timeline.map((song) => (
+                <TimelineItem
                 key={song.id}
                 song={song}
                 isDraggable={song.id === activeTimelineCard}
@@ -42,6 +35,6 @@ function Timeline({
         </div>
         </div>
     );
-    }
+}
 
 export default Timeline;
